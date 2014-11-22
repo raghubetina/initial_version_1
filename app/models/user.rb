f443f31feb004f15a8383b9ae137193908a98daf
Raghu Betina <rbetina@github.com>
2014-11-22 15:33:53 -0600

    Better sign-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider     = auth.provider
-      user.uid          = auth.uid
-      user.name         = auth.info.name
-      user.nickname     = auth.info.nickname
-      user.avatar_url   = auth.info.image
-      user.access_token = auth.credentials.token
-      user.save
-    end
+    user = find_or_initialize_by(provider: auth.provider, nickname: auth.info.nickname)
+    user.provider     = auth.provider
+    user.nickname     = auth.info.nickname
+    user.uid          = auth.uid
+    user.email        = auth.info.email
+    user.name         = auth.info.name
+    user.avatar_url   = auth.info.image
+    user.access_token = auth.credentials.token
+    user.save
+    user
   end
 end
